f10683948625930a0641fead05a3d44c3621cd02
Alex Kiura <kiuraalex@gmail.com>
2021-02-23 08:40:18 +0300

    Create counter app with useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useReducer } from 'react';
 
-function App() {
+
+const initialState = {
+  count1: 0,
+  count2: 0,
+  count3: 0
+}
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        [action.name]: state[action.name] + 1,
+      };
+    case 'DECREMENT':
+      return {
+        ...state,
+        [action.name]: state[action.name] - 1,
+      };
+    default:
+      return state
+  }
+}
+
+const useValue = () => useReducer(reducer, initialState);
+
+const Context = createContext(null);
+
+const useGlobalState = () => {
+  const value = useContext(Context);
+  if (value == null) throw new Error('Please add GlobalStateProvider');
+  return value;
+}
+
+const GlobalStateProvider = ({ children }) => (
+  <Context.Provider value={useValue()}>{children}</Context.Provider>
+);
+
+const Counter = ({ name }) => {
+  const [state, dispatch] = useGlobalState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {state[name]}
+      <button onClick={() => dispatch({ type: 'INCREMENT', name})}>+1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', name})}>-1</button>
     </div>
   );
-}
+
+};
+
+
+const App = () => (
+  <GlobalStateProvider>
+    <h1>Count1</h1>
+    <Counter name="count1"/>
+    <Counter name="count1"/>
+    <h1>Count2</h1>
+    <Counter name="count2"/>
+    <Counter name="count2"/>
+    <h1>Count3</h1>
+    <Counter name="count3"/>
+    <Counter name="count3"/>
+  </GlobalStateProvider>
+);
 
 export default App;
